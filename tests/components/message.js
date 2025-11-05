d8903bd38c0bf68ff9ad90fa227f4b749f0cd53a
lucas-tiago88 <lucas.tiago@atlastechnol.com>
2025-11-05 11:42:31 -0300

    feat: add swipe e outros casos de testes

--- tests/components/message.js
+++ tests/components/message.js
@@ -5,10 +5,11 @@ output.messagesValidation = {
     emailError: 'Please enter a valid email address',
     passwordError: 'Please enter at least 8 characters',
     loginSuccess: 'You are logged in!',
+    registerSuccess: 'You successfully signed up!',
     confirmPasswordError: 'Please enter the same password',
 };
 
-// função de validação que compara a mensagem atual com o esperado
-function validateLoginSuccess(actualMessage) {
-    return actualMessage === output.assertsLogin.loginSuccess;
-}
+// // função de validação que compara a mensagem atual com o esperado
+// function validateLoginSuccess(actualMessage) {
+//     return actualMessage === output.assertsLogin.loginSuccess;
+// }
